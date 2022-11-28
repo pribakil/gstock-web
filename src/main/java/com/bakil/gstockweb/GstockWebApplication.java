b7646ee59fe1d81c4f78fe80eb7212b95af2c9bc
pribakil <bakengailunga@gmail.com>
2022-11-28 15:53:42 +0100

    init

---
+++ src/main/java/com/bakil/gstockweb/GstockWebApplication.java
@@ -0,0 +1,13 @@
+package com.bakil.gstockweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GstockWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GstockWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bakil/gstockweb/GstockWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bakil.gstockweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GstockWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
